73d8d929795ef8b431fc7b398e98899abf0248c7
Travis St. Onge <MacBook@macbook.olin.edu>
2011-02-24 04:38:12 -0500

    fixed stuff

--- src/com/jamsesh/MyJams.java
+++ src/com/jamsesh/MyJams.java
@@ -2,11 +2,13 @@ package com.jamsesh;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MyJams extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myjams);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
 }
