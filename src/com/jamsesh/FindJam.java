5f9b2176647f5b0e11c53bd525fa851f333bbb78
Jon McKay <jonathan.mckay@students.olin.edu>
2011-02-24 04:40:46 -0500

    Finishing up myjams

--- src/com/jamsesh/FindJam.java
+++ src/com/jamsesh/FindJam.java
@@ -9,3 +9,5 @@ public class FindJam extends Activity{
         setContentView(R.layout.findjam);
     }
 }
+
+ 
--- src/com/jamsesh/MyJams.java
+++ src/com/jamsesh/MyJams.java
@@ -1,12 +1,27 @@
 package com.jamsesh;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class MyJams extends Activity{
     public void onCreate(Bundle savedInstanceState) {
+    	EditText hosting1;
+    	EditText hosting2;
+    	EditText attending1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myjams);
-    }
+        OnClickListener eventClicked = new OnClickListener() {
 
+    		@Override
+    		public void onClick(View v) {
+    			Intent i = new Intent(MyJams.this, JamInfo.class);
+    			startActivity(i);    			
+    		}
+        	
+        };
+    }
 }
