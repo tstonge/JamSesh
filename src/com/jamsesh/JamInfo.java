327a0c84c33a4945ceb9949fbbb7c7039cb9130a
Jon McKay <jonathan.mckay@students.olin.edu>
2011-02-24 05:44:02 -0500

    Added all base mock-up functionality

--- src/com/jamsesh/JamInfo.java
+++ src/com/jamsesh/JamInfo.java
@@ -2,14 +2,18 @@ package com.jamsesh;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class JamInfo extends Activity{
 	TextView title;
 	Integer parent;
 	LinearLayout ll;
+	Button attend;
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jaminfo);
@@ -18,15 +22,26 @@ public class JamInfo extends Activity{
         title.setText(getIntent().getStringExtra("com.jamsesh.title"));
         parent = getIntent().getIntExtra("com.jamsesh.parent", 1);
         if (parent == 0) {
-        	Button attend = new Button(this);
+        	attend = new Button(this);
         	attend.setText("Already Attending");
         	attend.setClickable(false);
+        	attend.setVisibility(4);
         	ll.addView(attend);
         } else {
-        	Button attend = new Button(this);
+        	attend = new Button(this);
         	attend.setText("Attend this Jam Sesh");
         	attend.setClickable(true);
         	ll.addView(attend);
         }
+        attend.setOnClickListener(attendClick);
 	}
+	
+	OnClickListener attendClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Toast.makeText(JamInfo.this, "Jam added to you MyJams Page!", Toast.LENGTH_LONG).show();			
+		}
+		
+	};
 }
